2f29a395940711a6fc6141ece4c479a193d285a5
nandirb <nandir.be@gmail.com>
2023-04-07 13:05:00 +0800

    ..

--- movies-backend/controllers/moviesController.ts
+++ movies-backend/controllers/moviesController.ts
@@ -2,8 +2,20 @@ import { Request, Response } from "express";
 import Movie from "../model/Movie";
 
 const getMovies = async (req: Request, res: Response) => {
-  const movies = await Movie.find().limit(10);
-  res.status(200).json({ success: true, movies });
+  console.log(req)
+  const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
+  const page = req.query.page ? parseInt(req.query.page as string) : 1;
+  const total = await Movie.countDocuments();
+  const pageCount = Math.ceil(total / limit);
+  const skip = (page - 1) * limit;
+  console.log("limit", limit, "page", page, "total", total, "pageCount", pageCount, "skip", skip)
+  const movies = await Movie.find({ poster: { $exists: true } }).skip(skip).limit(limit)
+  res.status(200).json({ success: true, movies , pagination: { page, pageCount, limit, total }});
 };
 
-export { getMovies };
+const getMovie = async (req: Request, res: Response) => {
+  const movie = await Movie.findOne({ _id: req.params.id }).limit(1);
+  res.status(200).send(movie);
+};
+
+export { getMovies, getMovie};
--- movies-backend/routes/moviesRouter.ts
+++ movies-backend/routes/moviesRouter.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { getMovies , getMovie} from "../controllers/moviesController";
+import { getMovie, getMovies } from "../controllers/moviesController";
 
 const router = Router();
 
 router.route("/").get(getMovies);
 router.route("/:id").get(getMovie);
-
 export default router;
